ffeadc8fbee84488630ff126e08d78ad1bc8daed
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-11-25 13:06:48 -0400

    Chore: generate types and hooks based on GQL queries

---
+++ src/queries/generated/hooks.tsx
@@ -0,0 +1,55 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+
+export const RepositoryDocument = gql`
+    query REPOSITORY($owner: String!, $name: String!) {
+  repository(owner: $owner, name: $name) {
+    id
+    nameWithOwner
+    pullRequests(last: 5) {
+      totalCount
+      nodes {
+        id
+        title
+        url
+      }
+    }
+    stargazerCount
+    issues(last: 5) {
+      totalCount
+      nodes {
+        id
+        title
+        url
+      }
+    }
+  }
+}
+    `;
+
+/**
+ * __useRepositoryQuery__
+ *
+ * To run a query within a React component, call `useRepositoryQuery` and pass it any options that fit your needs.
+ * When your component renders, `useRepositoryQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useRepositoryQuery({
+ *   variables: {
+ *      owner: // value for 'owner'
+ *      name: // value for 'name'
+ *   },
+ * });
+ */
+export function useRepositoryQuery(baseOptions: Apollo.QueryHookOptions<RepositoryQuery, RepositoryQueryVariables>) {
+        return Apollo.useQuery<RepositoryQuery, RepositoryQueryVariables>(RepositoryDocument, baseOptions);
+      }
+export function useRepositoryLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<RepositoryQuery, RepositoryQueryVariables>) {
+          return Apollo.useLazyQuery<RepositoryQuery, RepositoryQueryVariables>(RepositoryDocument, baseOptions);
+        }
+export type RepositoryQueryHookResult = ReturnType<typeof useRepositoryQuery>;
+export type RepositoryLazyQueryHookResult = ReturnType<typeof useRepositoryLazyQuery>;
+export type RepositoryQueryResult = Apollo.QueryResult<RepositoryQuery, RepositoryQueryVariables>;
