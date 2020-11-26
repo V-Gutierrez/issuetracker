17d22fc937e3ab4de06d8638e8b2a238a626c8c2
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-11-26 12:02:29 -0400

    Chore: implement Repositories Component

--- src/components/views/Repositories/index.tsx
+++ src/components/views/Repositories/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import RepositoryCard from 'components/modules/RepositoryCard';
 
-function Repositories() {
-  return <div />;
+interface RepositoryNameAndOwnerDTO {
+  name: string;
+  owner: string;
+}
+
+interface RepositoriesProps {
+  repositories: RepositoryNameAndOwnerDTO[];
+}
+
+function Repositories({ repositories }: RepositoriesProps) {
+  return (
+    <span className="repositories" data-testid="repositories-wrapper">
+      {repositories.map(({ name, owner }) => (
+        <RepositoryCard key={name + owner} name={name} owner={owner} />
+      ))}
+    </span>
+  );
 }
 
 export default Repositories;
---
+++ src/tests/components/views/Repositories.test.tsx
@@ -0,0 +1,22 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen } from '@testing-library/react';
+import Repositories from 'components/views/Repositories';
+import { queryMock } from 'tests/mocks';
+
+describe('Repositories View Component', () => {
+  it('should render properly based on props', () => {
+    const mockedRepositories = [{ name: 'IssueTracker', owner: 'v-gutierrez' }];
+
+    render(
+      <MockedProvider mocks={queryMock} addTypename={false}>
+        <Repositories repositories={mockedRepositories} />
+      </MockedProvider>
+    );
+
+    const wrapper = screen.getByTestId('repositories-wrapper');
+
+    expect(wrapper).toBeTruthy();
+    expect(wrapper.hasChildNodes()).toBe(true);
+    expect(wrapper.children.length).toEqual(mockedRepositories.length);
+  });
+});
