31f6aafef5cd9b74038492b00d37fc748247b984
v-gutierrez <victorgutierrezgomes@gmail.com>
2020-11-30 17:29:07 -0400

    Chore: implement StatsCard component

---
+++ src/tests/components/modules/StatsCard.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import StatsCard from 'components/modules/StatsCard';
+
+describe('StatsCard component', () => {
+  it('should render properly based on props', () => {
+    const mockedProps = {
+      id: '_id_',
+      title: 'Repository Issue',
+      url: '_url_'
+    };
+
+    render(<StatsCard {...mockedProps} type="issue" />);
+
+    const wrapper = screen.getByTestId('statscard-wrapper');
+    const linkToUrl = screen.getByTestId('statscard-anchor');
+    const title = screen.getByTestId('statscard-title');
+
+    expect(wrapper).toHaveClass('issue__card');
+    expect(linkToUrl).toHaveAttribute('href', mockedProps.url);
+    expect(title).toHaveTextContent(mockedProps.title);
+  });
+
+  it('should render properly based on props', () => {
+    const mockedProps = {
+      id: '_id_',
+      title: 'Repository PR',
+      url: '_url_'
+    };
+
+    render(<StatsCard {...mockedProps} type="pull_request" />);
+
+    const wrapper = screen.getByTestId('statscard-wrapper');
+    const linkToUrl = screen.getByTestId('statscard-anchor');
+    const title = screen.getByTestId('statscard-title');
+
+    expect(wrapper).toHaveClass('pullrequest__card');
+    expect(linkToUrl).toHaveAttribute('href', mockedProps.url);
+    expect(title).toHaveTextContent(mockedProps.title);
+  });
+});
